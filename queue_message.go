0cfcde8fd52e628d443201ccb07e3b8d349f3686
liyiwen <johnnyeven@gmail.com>
2020-09-29 15:37:18 +0800

    init

--- queue_message.go
+++ queue_message.go
@@ -1,6 +1,10 @@
 package common
 
-import "time"
+import (
+	"bytes"
+	"encoding/gob"
+	"time"
+)
 
 type QueueMessage struct {
 	// Topic is reads only and MUST NOT be set when writing messages
@@ -11,4 +15,21 @@ type QueueMessage struct {
 	Key       []byte
 	Val       []byte
 	Time      time.Time
+}
+
+func (q *QueueMessage) UnmarshalBinary(data []byte) error {
+	buf := bytes.NewReader(data)
+	decoder := gob.NewDecoder(buf)
+	return decoder.Decode(q)
+}
+
+func (q QueueMessage) MarshalBinary() (data []byte, err error) {
+	buf := bytes.NewBuffer([]byte{})
+	encoder := gob.NewEncoder(buf)
+	err = encoder.Encode(q)
+	if err != nil {
+		return
+	}
+	data = buf.Bytes()
+	return
 }
